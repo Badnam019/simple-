704afe5ccc2bf84b119593dcc4e0969045e97ffa
Badnam xd <yadavxd746@gmail.com>
2025-03-19 11:06:36 +0530

    Update play.py

--- PROMUSIC/utils/inline/play.py
+++ PROMUSIC/utils/inline/play.py
@@ -69,10 +69,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="˹ ❍ᴡɴᴇꝛ ˼", url=f"https://t.me/Legend_mickey"
+                text="˹ ❍ᴡɴᴇꝛ ˼", url=f"https://t.me/TEAM_BADNAM_BOTS"
             ),
             InlineKeyboardButton(
-                text="˹ 𝐒υρροяτ ˼", url=f"https://t.me/The_Incricible"
+                text="˹ 𝐒υρροяτ ˼", url=f"https://t.me/YASH_ABOUT_XD_lll"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
